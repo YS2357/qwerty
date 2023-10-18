2db5e783a18463038198225bda104fc844f8a957
YS2357 <130207967+YS2357@users.noreply.github.com>
2023-10-18 16:13:13 +0900

    Delete 3. for문 directory

--- 3. for문/3. sum.py
+++
@@ -1,8 +0,0 @@
-n = int(input())
-total = 0
-for i in range(1,n+1):
-    total += i
-print(total)
-
-# print(sum(range(1, int(input())+1)))
-print(sum(range(1, n+1)))
--- 3. for문/7. A+B - 7.py
+++
@@ -1,8 +0,0 @@
-t = int(input())
-for i in range(1, t+1):
-    a, b = map(int, input().split())
-    print("Case #"+str(i)+ ":", a+b)
-
-for i in range(1, t+1):  # 1부터 t까지
-    a, b = map(int, input().split())
-    print(f'Case #{i}: {a+b}')
